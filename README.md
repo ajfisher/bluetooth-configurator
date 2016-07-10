c622848d0beeea1e5bd5334930b9795ecc51c120
ajfisher <ajfisher.td@gmail.com>
2016-07-10 21:04:34 +1000

    initial commit with basic working code

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # bluetooth-configurator
+
 Configures bluetooth serial modules quickly from the command line.
+
+## Installation
+
+A simple install from npm should do the job.
+
+```
+npm install btconfigurator
+```
+
+## Wire up your Bluetooth module.
+
+You'll need a USB to serial converter to talk to the HC-06 module. Wire
+power & ground to the module an TX->RX between the module and the serial converter.
+
+## Usage
+
+Pass in the port, bt module name and pin number you want.
+
+```
+bt-config /dev/tty.usbserial01 testname 1234
+```
+
+Would use the BT module on port `/dev/tty.usbserial01` and give it the name
+`testname` and the pin `1234`
+
+If everything worked you should now be able to look for the bluetooth module
+from your bluetooth on your computer.
+
+## TODO:
+
+* Auto Baud detection
+* Different baud rates detected
+* Default pins
+* better error handling
+* Make serial instructions cleaner
---
+++ bt.js
@@ -0,0 +1,197 @@
+#! /usr/bin/env node
+
+var program = require("commander");
+var SerialPort = require("serialport");
+
+var sp = null;
+var error = false;
+var state = "SETUP";
+var response_buffer = "";
+
+var bt_name, bt_pin;
+
+var testConnection = function() {
+    return new Promise(function(ok, err) {
+        console.log("Testing the connection");
+        sp.write("AT", function(err) {
+            if (err) {
+                error = true;
+                throw (err);
+            }
+            state = "TEST_CONNECT_RESPONSE";
+
+            sp.drain(function(){
+                ok();
+            });
+        });
+    })
+};
+
+var setName = function(name) {
+    return new Promise(function(ok, err) {
+        console.log("Setting the name");
+        sp.write("AT+NAME" + name, function(err) {
+            if (err) {
+                error = true;
+                throw(err);
+            }
+            state = "SET_NAME_RESPONSE";
+
+            sp.drain(function() {
+                ok();
+            });
+        });
+    });
+};
+
+var setPin = function(pin) {
+    return new Promise(function(ok, err) {
+        console.log("Setting the pin");
+        sp.write("AT+PIN" + pin, function(err) {
+            if (err) {
+                error = true;
+                throw(err);
+            }
+            state = "SET_PIN_RESPONSE";
+
+            sp.drain(function() {
+                ok();
+            });
+        });
+    });
+};
+
+var setBaud = function() {
+    return new Promise(function(ok, err) {
+        console.log("Setting the Baudrate to 57600");
+        sp.write("AT+BAUD7", function(err) {
+            if (err) {
+                error = true;
+                throw(err);
+            }
+
+            state = "SET_BAUD_RESPONSE";
+
+            sp.drain(function() {
+                ok();
+            });
+        });
+    });
+};
+
+var closeConnection = function(err) {
+
+    if(err) {
+        console.log("An error occurred: ", err);
+    };
+
+    sp.close(function(err) {
+        if (err) {
+            console.log("Error closing port: ", err);
+            process.exit(1);
+        }
+
+        if (! error) {
+            console.log("You should now be able to connect with bluetooth" +
+                    " and see a module named %s which you can connect to" +
+                    " using PIN <%s>", bt_name, bt_pin);
+            process.exit(0);
+        }
+    });
+};
+
+
+function configure(port, module_name, pin) {
+    // does the writing of the module.
+
+    bt_name = module_name;
+    bt_pin = pin;
+
+    console.log("Opening serial port: %s", port);
+    sp = new SerialPort(port, {
+        baudRate: 9600,
+    });
+
+    sp.on('open', function(err) {
+        console.log("Serialport open");
+
+        testConnection()
+            .catch(function(err) {
+                console.log("error of some sort", err);
+                closeConnection(err);
+            });
+    });
+
+    sp.on('error', function(err) {
+        console.log("error opening port %s", err);
+    });
+
+    sp.on('data', function(data) {
+        response_buffer += data.toString();
+
+        switch (state) {
+            case "TEST_CONNECT_RESPONSE":
+                if (response_buffer == "OK") {
+                    console.log("Connection returned OK");
+                    response_buffer = "";
+                    setName(bt_name)
+                        .catch(function(err) {
+                            console.log("error of some sort", err);
+                            closeConnection(err);
+                        });
+                }
+                break;
+
+            case "SET_NAME_RESPONSE":
+                if (response_buffer == "OKsetname") {
+                    console.log("Set the name correctly");
+                    response_buffer = "";
+                    setPin(bt_pin)
+                        .catch(function(err) {
+                            console.log("error of some sort", err);
+                            closeConnection(err);
+                        });
+                }
+                break;
+
+            case "SET_PIN_RESPONSE":
+                if (response_buffer == "OKsetPIN") {
+                    console.log("Set the pin correctly");
+                    response_buffer = "";
+                    setBaud()
+                        .catch(function(err) {
+                            console.log("error of some sort", err);
+                            closeConnection(err);
+                        });
+                }
+                break;
+            case "SET_BAUD_RESPONSE":
+                if (response_buffer == "OK57600") {
+                    console.log("Set the baudrate");
+                    closeConnection();
+                }
+                break;
+        }
+    });
+}
+
+
+program
+    .version("0.0.1")
+    .usage("<port> <name> <pin>")
+    .description("Configure a BT serial module")
+    .action(function(port, module_name, pin) {
+        if (typeof(port) != "string" || typeof(module_name) != "string" || typeof(pin) != "string") {
+            program.help();
+        } else {
+            configure(port, module_name, pin);
+        }
+    });
+
+program.parse(process.argv);
+
+
+
+
+
+
